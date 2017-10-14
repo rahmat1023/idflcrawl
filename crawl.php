1506d825c71c76ecfb5d4755d44ffc68dba60c9b
rahmat1023 <rahmat1023@gmail.com>
2017-10-14 11:21:26 +0700

    Update crawl.php

--- crawl.php
+++ crawl.php
@@ -82,8 +82,10 @@ echo '	<tr>
 }
 $prev=$a-20;
 $next=$a+20;
+echo '<div>';
 echo ' <a href="crawl.php?a='.$prev.'">Sebelum</a>';
 echo '-----<a href="index.php?">Home</a>-----';
 echo ' <a href="crawl.php?a='.$next.'">Sesudah</a>';
+echo '</div>';
 ?>
 </html>
