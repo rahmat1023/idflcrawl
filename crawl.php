fb3626660ba5d7e73a8a7533eb5333cf4a662e5a
rahmat1023 <rahmat1023@gmail.com>
2017-10-14 10:52:41 +0700

    Update crawl.php

--- crawl.php
+++ crawl.php
@@ -1,6 +1,36 @@
 <html>
+<head>
 <title> Rahmat's file.idfl.me Crawler </title>
-	
+<style>
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+
+td {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+}
+
+th {
+    border: 1px solid #dddddd;
+    text-align: center;
+    padding: 8px;
+}
+
+tr:nth-child(even) {
+    background-color: #dddddd;
+}
+
+h1 {
+	text-align: center;
+	color: #000;
+}
+</style>
+</head>
+
 <?php
 // function to get webpage title
 ini_set('max_execution_time', 300);
@@ -21,17 +51,35 @@ function getTitle($url) {
     $title = preg_match('/<title[^>]*>(.*?)<\/title>/ims', $page, $match) ? $match[1] : null;
     return $title;
 }
+
+function getOwner($url) {
+   $page = file_get_contents($url);  
+  
+	$string_awal   = '<td>';  
+	$string_akhir   = '</td>';  
+  
+	$pos_awal = strrpos($page, $string_awal);  
+	$pos_akhir = strrpos($page, $string_akhir);  
+	$pos_ambil = $pos_akhir-$pos_awal;
+
+	$owner = substr($page, $pos_awal+4, $pos_ambil);
+  
+	return $owner;
+}
 $x=getWeb($b,$a,$z);
 // get web page title
-echo 'File nomor '.$a.' sampai '.$z;
-echo '<ul>';
-for ($i=$a;$i<=$z;$i++){
-	echo '<li><a href="'.$x[$i]. '">'. getTitle($x[$i]).'</a></li>' ;
+echo '<h1>File nomor '.$a.' sampai '.$z.'</h1>';
+echo '<table>';
+echo '	<tr>
+    		<th>File Name</th>
+    		<th>Owner</th>
+  		</tr>';
+	for ($i=$a;$i<=$z;$i++){
+		echo '<tr>';
+		echo '<td><a href="'.$x[$i]. '">'. getTitle($x[$i]).'</a></td>' ;
+		echo '<td>'. getOwner($x[$i]).'</a></td>' ;
+		echo '</tr>';
 }
 echo '</ul>';
-// Output:
-// Title: PHP 5 Tutorial
-include 'footer.php';
 ?>
-	
 </html>
